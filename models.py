dbbd292e35906a743ba35daa4ad246c00f1f189d
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-06-18 16:16:34 -0700

    add Person and Subscription tables/models

--- models.py
+++ models.py
@@ -8,11 +8,29 @@ db = SQLAlchemy()
 def get_uuid():
 	return uuid.uuid4().hex
 
+class Subscription(db.Model):
+	__tablename__ = 'lister_subscriptions'
+	list_id = db.Column(HashColumn(length=32), db.ForeignKey(List.id), primary_key=True)
+	person_id = db.Column(HashColumn(length=32), db.ForeignKey(Person.id), primary_key=True)
 class List(db.Model):
 	__tablename__ = "lister_lists"
 	id = db.Column(HashColumn(length=32),
 						primary_key=True, default=get_uuid)
 	name = db.Column(db.String(80), unique=True, nullable=False)
+	members = db.relationship(
+		Person,
+		secondary=Subscription,
+		backref="lists")
+
+	# def __repr__(self):
+	# 	return '<List %r>' % self.id
+
+class Person(db.Model):
+	__tablename__ = "lister_people"
+	id = db.Column(HashColumn(length=32),
+						primary_key=True, default=get_uuid)
+	# https://stackoverflow.com/a/7717596/
+	email = db.Column(db.String(254), unique=True, nullable=False)
 
 	# def __repr__(self):
 	# 	return '<List %r>' % self.id
