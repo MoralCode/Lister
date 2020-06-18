f19f8d2805af8a8d03b2004e658291290726dc64
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-06-18 13:14:39 -0700

    add setup routine to init the database

--- lister.py
+++ lister.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import logging
 from models import db
+import sys
 
 app_logger = logging.getLogger('lister')
 
@@ -12,6 +13,15 @@ db.init_app(app)
 def index():
 	return render_template("index.html")
 
+
+
+if sys.argv[1] == "setup":
+# https://stackoverflow.com/a/46541219
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
+        print("Setup Complete.")
+        exit(0)
 
 if __name__ == "__main__":
 	app.run()
