8dcbfb4d264480e73197cf470794fc2fa96fdaf0
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-06-18 17:38:25 -0700

    rearrange model items so that stuff is properly defined when it needs to be

--- models.py
+++ models.py
@@ -11,23 +11,11 @@ table_prefix = os.getenv("LISTER_TABLE_PREFIX", "lister_")
 def get_uuid():
 	return uuid.uuid4().hex
 
-class Subscription(db.Model):
-	__tablename__ = table_prefix+'subscriptions'
-	list_id = db.Column(HashColumn(length=32), db.ForeignKey(List.id), primary_key=True)
-	person_id = db.Column(HashColumn(length=32), db.ForeignKey(Person.id), primary_key=True)
-	token = db.Column(HashColumn(length=32), default=get_uuid)
-	date_subscribed = db.Column(db.DateTime, default=datetime.utcnow())
-
-
 class List(db.Model):
 	__tablename__ = table_prefix+"lists"
 	id = db.Column(HashColumn(length=32),
 						primary_key=True, default=get_uuid)
 	name = db.Column(db.String(80), unique=True, nullable=False)
-	members = db.relationship(
-		Person,
-		secondary=Subscription,
-		backref="lists")
 
 	# def __repr__(self):
 	# 	return '<List %r>' % self.id
@@ -38,6 +26,17 @@ class Person(db.Model):
 						primary_key=True, default=get_uuid)
 	# https://stackoverflow.com/a/7717596/
 	email = db.Column(db.String(254), unique=True, nullable=False)
+	lists = db.relationship(
+		List,
+		secondary="Subscription",
+		backref="members")
 
 	# def __repr__(self):
 	# 	return '<List %r>' % self.id
+
+class Subscription(db.Model):
+	__tablename__ = table_prefix+'subscriptions'
+	list_id = db.Column(HashColumn(length=32), db.ForeignKey(List.id), primary_key=True)
+	person_id = db.Column(HashColumn(length=32), db.ForeignKey(Person.id), primary_key=True)
+	token = db.Column(HashColumn(length=32), default=get_uuid)
+	date_subscribed = db.Column(db.DateTime, default=datetime.utcnow())
