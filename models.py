cf5270584c2ed4f55ce16e7b4fe3771d08fc0eef
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-06-18 17:03:12 -0700

    add table prefix environment variable

--- models.py
+++ models.py
@@ -1,16 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
-import uuid
+import uuid, os
 from datetime import datetime
 from helpers.guid import HashColumn
 
 
 db = SQLAlchemy()
 
+table_prefix = os.getenv("LISTER_TABLE_PREFIX", "lister_")
+
 def get_uuid():
 	return uuid.uuid4().hex
 
 class Subscription(db.Model):
-	__tablename__ = 'lister_subscriptions'
+	__tablename__ = table_prefix+'subscriptions'
 	list_id = db.Column(HashColumn(length=32), db.ForeignKey(List.id), primary_key=True)
 	person_id = db.Column(HashColumn(length=32), db.ForeignKey(Person.id), primary_key=True)
 	token = db.Column(HashColumn(length=32), default=get_uuid)
@@ -18,7 +20,7 @@ class Subscription(db.Model):
 
 
 class List(db.Model):
-	__tablename__ = "lister_lists"
+	__tablename__ = table_prefix+"lists"
 	id = db.Column(HashColumn(length=32),
 						primary_key=True, default=get_uuid)
 	name = db.Column(db.String(80), unique=True, nullable=False)
@@ -31,7 +33,7 @@ class List(db.Model):
 	# 	return '<List %r>' % self.id
 
 class Person(db.Model):
-	__tablename__ = "lister_people"
+	__tablename__ = table_prefix+"people"
 	id = db.Column(HashColumn(length=32),
 						primary_key=True, default=get_uuid)
 	# https://stackoverflow.com/a/7717596/
